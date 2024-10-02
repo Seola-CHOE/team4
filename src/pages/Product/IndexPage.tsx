80391f3238a2ca13d995363c6cba3e265fc921f0
ssinyong <bsy2889@gmail.com>
2024-10-02 15:22:29 +0900

    ReadComponent 삭제

--- src/pages/Product/IndexPage.tsx
+++ src/pages/Product/IndexPage.tsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 function IndexPage() {
   
   return (
-      <Navigate to={'/products/list'}></Navigate>
+      <Navigate to={'/product/list'}></Navigate>
   );
 }
 
