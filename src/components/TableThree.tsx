6356a5f0a41e5111dc4ef62ad9a9bef94c262add
Seola CHOE <tjfdk0493@gmail.com>
2024-10-03 17:47:42 +0900

    17:47

--- src/components/TableThree.tsx
+++ src/components/TableThree.tsx
@@ -6,16 +6,19 @@ const TableThree = () => {
           <thead>
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
               <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
-                Package
+                Order
+              </th>
+              <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
+                Order
               </th>
               <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                Invoice date
+                Date
               </th>
               <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
                 Status
               </th>
               <th className="py-4 px-4 font-medium text-black dark:text-white">
-                Actions
+                Modi/Del
               </th>
             </tr>
           </thead>
--- src/components/todos/TodoListComponent.tsx
+++ src/components/todos/TodoListComponent.tsx
@@ -1,35 +1,67 @@
-import { ITodo } from '../../types/todo.ts';
-import useTodoList from '../../hooks/useTodoList.ts';
-import TableThree from "../TableThree.tsx";
+import useTodoList from "../../hooks/useTodoList.ts";
+import { ITodo } from "../../types/todo.ts";
+import PageComponent from '../../common/PageComponent.tsx';
 
 function TodoListComponent() {
+  const { pageResponse, moveToRead } = useTodoList();
 
-  const { pageResponse,moveToRead} = useTodoList()
+  const listLI = pageResponse.content.map((todo: ITodo) => {
+    const { mno, title, writer, dueDate } = todo;
 
-  const listLI = pageResponse.content.map( (todo:ITodo)=> {
-
-    const {mno, title, writer, dueDate} = todo
-
-    return(
-      <li key={mno} onClick={() => moveToRead(mno)}>
-        {mno} - {title} - {writer} - {dueDate}
-      </li>
-    )
-
-  })
+    return (
+      <tr
+        key={mno}
+        onClick={() => moveToRead(mno)}
+        className="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800"
+      >
+        <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
+          <h5 className="font-medium text-black dark:text-white">{mno}</h5>
+        </td>
+        <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+          <p className="text-black dark:text-white">{title}</p>
+        </td>
+        <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+          <p className="text-black dark:text-white">{writer}</p>
+        </td>
+        <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+          <p className="text-black dark:text-white">
+            {new Date(dueDate).toLocaleDateString()}
+          </p>
+        </td>
+        <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+          <button className="text-blue-500 hover:text-blue-700">Edit/Delete</button>
+        </td>
+      </tr>
+    );
+  });
 
   return (
-
-    <div>
-
-      <div>
-        TodoListComponent
+    <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="max-w-full overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead>
+          <tr className="bg-gray-2 text-left dark:bg-meta-4">
+            <th className="min-w-[220px] py-4 px-4 justify-center text-black dark:text-white xl:pl-11">
+              No.
+            </th>
+            <th className="min-w-[220px] py-4 px-4 justify-center text-black dark:text-white xl:pl-11">
+              Title
+            </th>
+            <th className="min-w-[150px] py-4 px-4 justify-center text-black dark:text-white">
+              Writer
+            </th>
+            <th className="py-4 px-4 justify-center text-black dark:text-white">
+              Due Date
+            </th>
+            <th className="py-4 px-4 justify-center text-black dark:text-white">
+              Modi/Del
+            </th>
+          </tr>
+          </thead>
+          <tbody>{listLI}</tbody>
+        </table>
       </div>
-
-      <ul>{listLI}</ul>
-
-    <TableThree pageResponse={pageResponse}/>
-
+      <PageComponent pageResponse={pageResponse} />
     </div>
   );
 }
