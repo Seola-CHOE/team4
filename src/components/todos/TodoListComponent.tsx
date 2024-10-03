55e806129797324024b75f588305e9bc19408ff1
USER <ckalswn371004@gmail.com>
2024-10-03 16:06:38 +0900

    modal 창 완료(업데이트 필요)_MJ

--- src/components/todos/TodoListComponent.tsx
+++ src/components/todos/TodoListComponent.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import useTodoList from '../../hooks/useTodoList';
 import { ITodo } from '../../types/todo';
-import ModifyComponent from '../todos/TodoModifyComponent'; // 모달 컴포넌트 가져오기
+import ModifyComponent from '../todos/TodoModifyComponent';
+import PageComponent from '../../common/PageComponent.tsx'; // 모달 컴포넌트 가져오기
 
 function TodoListComponent() {
   const { pageResponse, moveToRead } = useTodoList();
@@ -45,48 +46,48 @@ function TodoListComponent() {
   });
 
   return (
-    <div className="p-6 bg-white rounded-lg shadow-md">
-      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
-        <div className="max-w-full overflow-x-auto">
-          <table className="w-full table-auto border-collapse border border-gray-300">
-            <thead>
-            <tr className="bg-gray-300">
-              <th className="min-w-[110px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11 border-b border-gray-300 text-center">제목</th>
-              <th className="min-w-[110px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11 border-b border-gray-300 text-center">작가</th>
-              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white border-b border-gray-300 text-center">날짜</th>
-              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white border-b border-gray-300 text-center">작업</th>
-            </tr>
-            </thead>
-            <tbody>
-            {uniqueTodos.map((todo: ITodo) => {
-              const { mno, title, writer, dueDate } = todo;
+    <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="max-w-full overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead>
+          <tr className="bg-gray-2 text-left dark:bg-meta-4">
+            <th className="min-w-[10%] py-4 px-4 font-medium text-black dark:text-white xl:pl-11 text-center">NO.</th>
+            <th className="min-w-[30%] py-4 px-4 font-medium text-black dark:text-white xl:pl-11 text-center">TITLE</th>
+            <th className="min-w-[30%] py-4 px-4 font-medium text-black dark:text-white text-center">WRITER</th>
+            <th className="min-w-[30%] py-4 px-4 font-medium text-black dark:text-white text-center">DUEDATE</th>
+            <th className="min-w-[30%] py-4 px-4 font-medium text-black dark:text-white text-center">MODI/DEL</th>
+          </tr>
+          </thead>
+          <tbody>
+          {uniqueTodos.map((todo: ITodo) => {
+            const { mno, title, writer, dueDate } = todo;
 
-              return (
-                <tr key={mno} onClick={() => moveToRead(mno)} className="bg-gray-200 hover:bg-gray-100 border-b border-gray-300">
-                  <td className="py-4 px-4 text-center">{title}</td>
-                  <td className="py-4 px-4 text-center">{writer}</td>
-                  <td className="py-4 px-4 text-center">{dueDate}</td>
-                  <td className="py-4 px-4 text-center">
-                    <div className="flex items-center justify-center space-x-2">
-                      <button
-                        className="text-blue-500 hover:text-blue-700"
-                        onClick={(e) => {
-                          e.stopPropagation(); // 부모 요소의 클릭 이벤트 전파 차단
-                          openModifyModal(todo); // 수정 모달 열기
-                        }}
-                      >
-                        Modify
-                      </button>
-                      <div>/</div>
-                      <button className="text-red-500 hover:text-red-700">Delete</button>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-            </tbody>
-          </table>
-        </div>
+            return (
+              <tr key={mno} onClick={() => moveToRead(mno)} className="bg-gray-200 hover:bg-gray-100 border-b border-gray-300">
+                <td className="py-4 px-4 text-center">{mno}</td>
+                <td className="py-4 px-4 text-center">{title}</td>
+                <td className="py-4 px-4 text-center">{writer}</td>
+                <td className="py-4 px-4 text-center">{dueDate}</td>
+                <td className="py-4 px-4 text-center">
+                  <div className="flex items-center justify-center space-x-2">
+                    <button
+                      className="text-blue-500 hover:text-blue-700"
+                      onClick={(e) => {
+                        e.stopPropagation(); // 부모 요소의 클릭 이벤트 전파 차단
+                        openModifyModal(todo); // 수정 모달 열기
+                      }}
+                    >
+                      Modify
+                    </button>
+                    <div>/</div>
+                    <button className="text-red-500 hover:text-red-700">Delete</button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </table>
       </div>
 
       {/* Modify Modal */}
@@ -97,6 +98,7 @@ function TodoListComponent() {
           onModify={handleModify} // handleModify 전달
         />
       )}
+      <PageComponent pageResponse={pageResponse} />
     </div>
   );
 }
