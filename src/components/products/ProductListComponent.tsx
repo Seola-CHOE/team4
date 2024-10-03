8f6e348875ccca6706764e903af1ad63446b7500
eggzuxi <eggzuxi99@gmail.com>
2024-10-03 17:51:42 +0900

    forPull_JH

--- src/components/products/ProductListComponent.tsx
+++ src/components/products/ProductListComponent.tsx
@@ -60,7 +60,7 @@ function ProductListComponent() {
       <table className="min-w-full table-auto">
         <thead>
         <tr className="bg-gray-100 text-left">
-          {/*<th className="px-4 py-2 font-semibold text-gray-600">Product</th>*/}
+          <th className="px-4 py-2 font-semibold text-gray-600">Product</th>
           <th className="px-4 py-2 font-semibold text-gray-600">Product Name</th>
           <th className="px-4 py-2 font-semibold text-gray-600">Category</th>
           <th className="px-4 py-2 font-semibold text-gray-600">Price</th>
@@ -70,7 +70,7 @@ function ProductListComponent() {
         {productList.length > 0 ? (
           productList.map((product) => (
             <tr onClick={() => moveToRead(product.pno)} key={product.pno} className="border-b border-gray-200">
-              {/*<td className="px-4 py-4 text-gray-700 font-medium">{product.uploadFileNames(product.pno)}</td>*/}
+              <td className="px-4 py-4 text-gray-700 font-medium"><img className="w-1/12 h-full" src={`http://localhost:8089/api/products/view/s_${product.uploadFileNames[0]}`} alt="" /></td>
               <td className="px-4 py-4 text-gray-700 font-medium">{product.pname}</td>
               <td className="px-4 py-4 text-gray-600">{product.pdesc}</td>
               <td className="px-4 py-4 text-gray-700">${product.price.toLocaleString()}</td>
