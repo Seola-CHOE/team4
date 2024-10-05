d881e26ca85741eae29eb5cc2495ac962a52bf79
sungoh <sungoh@gmail.com>
2024-10-05 22:09:03 +0900

    todoAPI.ts,TodoReadComponent(수정) _SO

--- src/common/PageComponent.tsx
+++ src/common/PageComponent.tsx
@@ -12,62 +12,61 @@ const makeArr = (from: number, to: number): number[] => {
     for (let i = from; i <= to; i++) {
         arr.push(i);
     }
-    return arr
+    return arr;
 }
 
-function PageComponent({pageResponse}: Props): ReactElement {
+function PageComponent({pageResponse}: Props): ReactElement | null { // 리턴 타입에 null 추가
 
-    const current: number = pageResponse.current + 1
-    //  (1/10.0) => 0.1 => 1 * 10 => 20
-    const tempLast: number = Math.ceil(current / 10.0) * 10
-    const startPage: number = tempLast - 9
+    // 페이지가 없을 경우 컴포넌트 렌더링하지 않음
+    if (pageResponse.totalPage === 0) return null;
+
+    const current: number = pageResponse.current + 1;
+    const tempLast: number = Math.ceil(current / 10.0) * 10;
+    const startPage: number = tempLast - 9;
     const endPage: number = pageResponse.totalPage < tempLast ? pageResponse.totalPage : tempLast;
-    const prev:boolean = startPage !== 1
-    const next:boolean = true
+    const prev:boolean = startPage !== 1;
+    const next:boolean = endPage < pageResponse.totalPage; // 수정: next가 totalPage보다 작아야 true
 
-    const pageNums: number[] = makeArr(startPage, endPage)
+    const pageNums: number[] = makeArr(startPage, endPage);
 
-    const [query,setQuery] = useSearchParams()
+    const [query,setQuery] = useSearchParams();
 
     const changePage = (pageNum: number) => {
-
-        query.set("page", String(pageNum))
-        setQuery(query)
-
+        query.set("page", String(pageNum));
+        setQuery(query);
     }
 
     const lis = pageNums.map(num => <li
-        className='px-4 py-2 text-#64748b bg-blue-500 border border-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
-        key={num}
-        onClick={() => changePage(num)}
+      className='px-4 py-2 text-#64748b bg-blue-500 border border-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
+      key={num}
+      onClick={() => changePage(num)}
     >
         {num}
-    </li>)
-
+    </li>);
 
     return (
-        <div>
-            <ul className='flex justify-center items-center space-x-2 mt-6'>
+      <div>
+          <ul className='flex justify-center items-center space-x-2 mt-6'>
 
-                { prev && <li
-                    className='px-4 py-2 text-#64748b bg-blue-500 border border-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
-                    key={startPage -1}
-                    onClick={() => changePage(startPage -1)}
-                >
-                    Prev
-                </li>}
+              { prev && <li
+                className='px-4 py-2 text-#64748b bg-blue-500 border border-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
+                key={startPage - 1}
+                onClick={() => changePage(startPage - 1)}
+              >
+                  Prev
+              </li>}
 
-                {lis}
-                { next && <li
-                    className='px-4 py-2 text-#64748b bg-blue-500 border border-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
-                    key={endPage +1}
-                    onClick={() => changePage(endPage + 1)}
-                >
-                    Next
-                </li>}
+              {lis}
+              { next && <li
+                className='px-4 py-2 text-#64748b bg-blue-500 border border-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
+                key={endPage + 1}
+                onClick={() => changePage(endPage + 1)}
+              >
+                  Next
+              </li>}
 
-            </ul>
-        </div>
+          </ul>
+      </div>
     );
 }
 
