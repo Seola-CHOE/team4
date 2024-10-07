52499064f3d41e12101346b8c26889e29c27e728
ssinyong <bsy2889@gmail.com>
2024-10-07 15:07:29 +0900

    카테고리 분류 완료 최종_SY

--- src/components/todos/TodoModifyComponent.tsx
+++ src/components/todos/TodoModifyComponent.tsx
@@ -28,8 +28,7 @@ const ModifyComponent: React.FC<ModifyComponentProps> = ({ todo, onUpdate, onClo
   };
 
 
-  const handleSave = () => {
-=======
+
     const handleSave = () => {
 
     onUpdate(updatedTodo);
